b3033c156418053b500bfd0a80c7e7aa40d73a8d
Alice Li <alli@c1r6p18.hive.fi>
2024-10-25 16:11:55 +0300

    cpp02

--- cpp02/ex00/Fixed.hpp
+++ cpp02/ex00/Fixed.hpp
@@ -6,13 +6,20 @@
 /*   By: alli <alli@student.hive.fi>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 16:23:33 by alli              #+#    #+#             */
-/*   Updated: 2024/10/24 16:27:00 by alli             ###   ########.fr       */
+/*   Updated: 2024/10/25 13:28:36 by alli             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FIXED_HPP
 #define FIXED_HPP
 
+#include <iostream>
+#include <ieee754.h>
 
 
+class Fixed {
+	private:
+	public:
+	
+}
 #endif
--- cpp02/ex00/main.cpp
+++ cpp02/ex00/main.cpp
@@ -6,8 +6,21 @@
 /*   By: alli <alli@student.hive.fi>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 16:23:38 by alli              #+#    #+#             */
-/*   Updated: 2024/10/24 16:26:33 by alli             ###   ########.fr       */
+/*   Updated: 2024/10/25 13:25:53 by alli             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
+
+int main( void ) {
+	Fixed a;
+	Fixed b( a );
+	Fixed c;
+	
+	c = b;
+
+	std::cout << a.getRawBits() << std::endl;
+	std::cout << b.getRawBits() << std::endl;
+	std::cout << c.getRawBits() << std::endl;
+	return 0;
+}
