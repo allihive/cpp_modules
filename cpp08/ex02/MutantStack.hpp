e7600ff803a1c0de7963a60cc73c6ab574f1a231
Alice Li <aliceli.bls@gmail.com>
2025-03-03 14:21:24 +0200

    added const iterator

--- cpp08/ex02/MutantStack.hpp
+++ cpp08/ex02/MutantStack.hpp
@@ -19,5 +19,18 @@ class MutantStack : public std::stack<T, Container> {
 
 		using iterator = typename Container::iterator;
 		using const_iterator = typename Container::const_iterator;
+
+		iterator begin() {
+			return this->c.begin();
+		}
+		iterator end() {
+			return this->c.end();
+		}
+		const_iterator begin() const {
+			return this->c.begin();
+		}
+		const_iterator end() const {
+			return this->c.end();
+		}
 };
 
