19a07aba0f2bdffd7c800e5237ca61546d2a7390
Alice Li <aliceli.bls@gmail.com>
2025-03-04 13:38:35 +0200

    added number to push

--- cpp08/ex02/main.cpp
+++ cpp08/ex02/main.cpp
@@ -10,8 +10,8 @@ int main()
 	std::cout << mstack.size() << std::endl;
 	mstack.push(3);
 	mstack.push(5);
+	mstack.push(10);
 	mstack.push(737);
-	//[...]
 	mstack.push(0);
 	MutantStack<int>::iterator it = mstack.begin();
 	MutantStack<int>::iterator ite = mstack.end();
