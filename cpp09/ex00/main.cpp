4895530a9f00490be862f92dc3692b864ae6dc80
Alice Li <aliceli.bls@gmail.com>
2025-04-03 16:52:28 +0300

    fixed main

--- cpp09/ex00/main.cpp
+++ cpp09/ex00/main.cpp
@@ -5,8 +5,13 @@ int main(int argc, char **argv)
 	if (argc != 2)
 	{
 		std::cerr << "Format error: ./btc <input file>" << std::endl;
+		return (-1);
 	}
-	Bitcoin bit;
-
-	bit.parseInputFile(argv[1]);
+	try {
+		Bitcoin bit;
+		bit.parseInputFile(argv[1]);
+	}
+	catch (const std::exception &e) {
+		std::cout << e.what() << std::endl;
+	}
 }
