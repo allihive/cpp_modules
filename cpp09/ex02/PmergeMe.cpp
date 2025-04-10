14ff47d13f014fb480e22109386c623ef3ee1e0e
Alice Li <aliceli.bls@gmail.com>
2025-04-10 11:11:32 +0300

    written the logic for sort vector

--- cpp09/ex02/PmergeMe.cpp
+++ cpp09/ex02/PmergeMe.cpp
@@ -30,6 +30,7 @@ bool PmergeMe::convertNumbers(const std::string &arg) {
 	try {
 		int num = stoi(arg);
 
+		//check for duplicates here
 		_dequeList.push_back(num);
 		_vectorList.push_back(num);
 		return true;
@@ -42,6 +43,43 @@ bool PmergeMe::convertNumbers(const std::string &arg) {
 	return true;
 }
 
-void PmergeMe::sortVector() {
+//write insertjacobsthal numbers
+//create a vector of the sequence to determine the index of numbers
+//create the sequence by checking the amount of pairs there are
+//find the first pair in the main vector (b1) and insert
+	//will insert it at the first place because it's naturally smaller
+//go through the sequence size
+	//sequence i (ex. 5)
+	//sequence i-1 (the last number that was in the sequence) (ex. 3)
+	//find the pair, based off index of the first number (like a map)
+	//find the position to insert using lower bound?
+	//then insert
+
+void PmergeMe::sortVector(std::vector<int> &vec) {
+	
+	bool oddNumber = false;
+
+	if (vec.size() <= 1)
+		return ;
+	std::vector<std::pair<int, int>> pairs; //create pairs for vector
+	for (size_t i = 0; i < vec.size() / 2; i += 2)
+	{
+		if (vec[i] < vec[i + 1])
+			std::swap(vec[i], vec[i + 1]);
+		pairs.emplace_back(vec[i], vec[i + 1]); //now a1 is bigger and placed into the vector of pairs
+	}
+	if (vec.size() % 2 != 0)
+	{
+		oddNumber = true;
+		int leftOver = vec.back();
+	}
+	
+	//push the pair into the pairs vector will need to increase the loop by 2
+	//create a vector for large numbers (a) 
+	//call the vec algo again
+	//insert using jacobsthal
+	//check the leftover
+		//find the position to insert
+	//main vector will now be large numbers
 
 }
--- cpp09/ex02/PmergeMe.hpp
+++ cpp09/ex02/PmergeMe.hpp
@@ -4,20 +4,25 @@
 #include <vector>
 #include <deque>
 #include <limits>
+#include <chrono>
 
 class PmergeMe {
 	private:
 		std::vector<int> _vectorList;
 		std::deque<int> _dequeList;
+		std::vector<int> _jacobSeq;
+
 	public:
 		PmergeMe();
 		~PmergeMe();
 		PmergeMe(const PmergeMe &other);
 		PmergeMe& operator=(const PmergeMe &other);
 
-		void sortVector();
-		void sortDeque();
+		void sortVector(std::vector<int> &vec);
+		void sortDeque(std::deque<int> &deq);
 		bool convertNumbers(const std::string &);
-		void getTime()
+
+		// std::chrono::duration<double, std::micro> timeVector();
+		// std::chrono::duration<double, std::micro> timeDeque();
 
 };
